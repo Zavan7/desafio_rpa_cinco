fdc9f7e23598990203d5262e17c2d4c21af0765d
Vitor Zavan <vitorzavanj@gmail.com>
2026-07-26 23:32:14 -0300

    reajustes dos logs

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ import logging
 from config.log import setup_logging
 from db.mongo import MongoDB
 
-
+# URL da aplicação usada no desafio
 url = 'https://practicetestautomation.com/'
+
+# Seletores utilizados durante a navegação
 page_patrice_selector = '#menu-item-20'
 page_exception_selector = "//a[text()='Test Exceptions']"
 button_add_selector  = '#add_btn'
@@ -27,16 +29,19 @@ input_selector  = '#row2 .input-field'
 button_save_selector = '#save_btn .bnt'
 tag_savad_locator = '#confirmation'
 
+# Texto utilizado para preencher a navegação
 text = 'Pastel de Frango'
-
 
 
 logger = logging.getLogger(__name__)
 
+# Responsável por registrar o resultado da automação
 mongo = MongoDB()
 
 def main() -> None:
 
+    # Estrutura que armazena os dados da execução para auditoria
+    # e posterior persistência no MongoDB.
     result = {
         'start_date': None,
         'end_date': None,
@@ -45,36 +50,36 @@ def main() -> None:
         'error': None,
     }
 
+    # Marca o inicio da execução da automação
     start_date = datetime.now(UTC)
 
     setup_logging()
     try:
-        '''
-        Iniciando objeto playwright
-        '''
+
+        # Inicializa o Playwright e abre uma nova sessão do navegador
         with sync_playwright() as p:
             browser = p.firefox.launch(headless=False)
             page = browser.new_page()
 
 
-            # 1º etapa
+            # Etapa 1 - Acessa a página inicial
             initial_page = InitialPage(page, url)
             initial_page.open()
 
 
-            # 2º etapa
+            # Etapa 2 - Navega até a página Patrice
             patrice_page = PatricePage(page, page_patrice_selector)
             patrice_page.click_patrice()
 
 
-            # 3º etapa
+            # Etapa 3 - Acessa a página Test Exceptions
             test_exception_page = TestExceptionPage(
                 page, page_exception_selector
             )
             test_exception_page.click_test_exception()
 
 
-            # 4º etapa
+            # Etapa 4 - Inicia o desafio
             test_challenge = TestExceptionChallenge(
                 page,
                 button_add_selector,
@@ -83,31 +88,36 @@ def main() -> None:
 
             test_challenge.test_exceptions_challenge()
 
-            # 5º Digitando no input do site
+            # 5º Etapa 5 - Preenche o campo de entrada
             test_challenge.input_challenge(text, input_selector)
 
-            # 6º Salvando informação adicionada
+            # Etapa 6 - Salva as alterações
             test_challenge.save_challenge(button_save_selector)
 
-            # 7º - Validação final
+            # Etapa 7 - Valida se a operação foi concluída com sucesso
             validacao = ValidationFinal(page, tag_savad_locator)
             validacao.validation_final()
 
+            # Encerra a sessão do navegador
             browser.close()
 
             status = 'Success'
 
             end_date = datetime.now(UTC)
 
+    # Registra falha durante a execução da automação
     except Exception as e:
         error = f'Error: {e}'
         status = 'fail'
 
         print(e)
 
-    finally:        
+    finally:
+
+        # Calcula o tempo total de execução da automação
         duration = (end_date - start_date).total_seconds()
 
+        # Atualziando informações que serão inseridas no banco de dados
         result.update({
             'start_date': start_date,
             'end_date': end_date,
@@ -115,7 +125,10 @@ def main() -> None:
             'status': status
         })
 
+        # Persiste o resultado da execução no banco de dados
         try:
+            
+            # Inserindo informações no banco de dados
             mongo.insert(result)
             logger.info('Salvo no banco de dados')
         
